3f373c543b85e08b2dc2bf9362758736e0534ecb
hatef <hatef.khodkar@hotmail.com>
2022-02-25 22:47:06 +0330

    add navbar and httpclient

--- Client/src/app/app.component.ts
+++ Client/src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { IPagination } from './models/pagination.model';
+import { IProduct } from './models/product.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'my-$hop';
+
+  products: IProduct[] = [];
+
+  constructor(private httpService: HttpClient) {}
+
+  ngOnInit() {
+    this.httpService
+      .get('https://localhost:5001/api/products?pageSize=50')
+      .subscribe({
+        next: (res:any) => this.products = res.data,
+        error: err => console.log(err)
+     });
+  }
 }
