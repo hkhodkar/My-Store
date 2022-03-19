00e5cc511d508ef688903179fab594c7dd7c12e0
hatef <hatef.khodkar@hotmail.com>
2022-03-19 14:59:33 +0330

    add carousel to home page

--- Client/src/app/shared/shared.module.ts
+++ Client/src/app/shared/shared.module.ts
@@ -3,10 +3,16 @@ import { CommonModule } from '@angular/common';
 import { PaginationModule } from 'ngx-bootstrap/pagination';
 import { PaginHeaderComponent } from './components/pagin-header/pagin-header.component';
 import { PagerComponent } from './components/pager/pager.component';
+import { CarouselModule } from 'ngx-bootstrap/carousel';
 
 @NgModule({
   declarations: [PaginHeaderComponent, PagerComponent],
-  imports: [CommonModule, PaginationModule.forRoot()],
-  exports: [PaginationModule, PaginHeaderComponent, PagerComponent],
+  imports: [CommonModule, PaginationModule.forRoot(), CarouselModule.forRoot()],
+  exports: [
+    PaginationModule,
+    PaginHeaderComponent,
+    PagerComponent,
+    CarouselModule,
+  ],
 })
 export class SharedModule {}
